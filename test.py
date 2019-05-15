ff4449d13866a735bad6fe59818035e3f84d4457
Adrian Meza <adrianm1@outlook.com>
2019-05-15 16:48:50 -0500

    Dockerfile for test

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import unittest
 import random
 import netpay_ecommerce
@@ -5,9 +7,9 @@ import netpay_ecommerce
 class BaseSetupTest(unittest.TestCase):
 
     client = netpay_ecommerce
-    username = "adrian@netpay.com"
+    username = "edwin.rivas@netpay.com.mx"
     password = "adm0n2"
-    #netpay_ecommerce.BASE_URL = "Host"
+    netpay_ecommerce.BASE_URL = "https://cert.netpay.com.mx"
     
 
     login_object = {
@@ -18,103 +20,126 @@ class BaseSetupTest(unittest.TestCase):
     }
 
     token_card_object = {
-        "username": "adrian@netpay.com",
-        "storeApiKey": "6kQui=_ZJ4r15GRT5ix7Pot_sCObk1WG",
+        "username": "edwin.rivas@netpay.com.mx",
+        "storeApiKey": "QX!zPs1bq5uqMCUSd_IX=W7_Rpp=Byds",
         "customerCard": {
-            "cardNumber": "4000000000000007",
+            "cardNumber": "4000000000000002",
             "expirationMonth": "01",
             "expirationYear":  "24",
             "cvv": "123",
             "cardType": "001",
             "cardHolderName": "John Doe"
         }
-    }
+        }
 
     risk_manager_object = {  
-        "storeApiKey":"6kQui=_ZJ4r15GRT5ix7Pot_sCObk1WG",
-        "riskManager":{
-            "promotion":"000000",
-            "requestFraudService":{  
-                "merchantReferenceCode":"14500049450000",
-                "deviceFingerprintID":"086e46a7d7b0d22a8966feed39eefd1c881507939638",
-                "bill":{  
-                "city":"city",
-                "country":"MX",
-                "firstName":"mailto",
-                "lastName":"mailto",
-                "email":"accept@netpay.com.mx",
-                "phoneNumber":"8110000011",
-                "postalCode":"12345",
-                "state":"state",
-                "street1":"street 1",
-                "street2":"street 2",
-                "ipAddress":"10.0.0.1"
-                },
-                "ship":{  
-                "city":"city ship",
-                "country":"MX",
-                "firstName":"mailto",
-                "lastName":"mailto",
-                "phoneNumber":"8110111111",
-                "postalCode":"12345",
-                "state":"state",
-                "street1":"street 1",
-                "street2":"street 2",
-                "shippingMethod":"flatrate_flatrate"
-                },
-                "itemList":[{  
-                    "id":"421",
-                    "productSKU":"wbk012c-Royal Blue-S",
-                    "unitPrice":"1.0000",
-                    "productName":"Elizabeth Knit Top",
-                    "quantity":1,
-                    "productCode":"Tops & Blouses"
-                }
-                ],
-                "card":{  
-                "cardToken":"VW2FdHVU6BXGoiR99teVRKnuvIE897u7AMlh8MVHprBr4/LHIv6r7Nn0SwNEfsEfx8i7ngLOZyEL+eLsKoZCGg=="
-                },
-                "purchaseTotals":{  
-                "grandTotalAmount":"6",
-                "currency":"MXN"
-                },
-                "merchanDefinedDataList":[{  
-                    "id":2,
-                    "value":"Web"
-                },{  
-                    "id":4,
-                    "value":"515"
-                },{  
-                    "id":5,
-                    "value":"0"
-                },{  
-                    "id":6,
-                    "value":"0"
-                },{ 
-                    "id":7,
-                    "value":"0"
-                },{  
-                    "id":9,
-                    "value":"Retail"
-                },{  
-                    "id":10,
-                    "value":"3D"
-                },{  
-                    "id":11,
-                    "value":"flatrate_flatrate"
-                },{  
-                    "id":13,
-                    "value":"N"
-                },{  
-                    "id":14,
-                    "value":"Domicilio"
-                },{  
-                    "id":"16",
-                    "value":"50000"
-                }]
-            }
-        }   
-    }
+  "storeApiKey":"QX!zPs1bq5uqMCUSd_IX=W7_Rpp=Byds",
+  "riskManager":{
+     "promotion":"000000",
+     "requestFraudService":{  
+        "merchantReferenceCode":"14500056",
+        "deviceFingerprintID":"fea7f7888a184a10ae2820969ef3062a",
+        "bill":{  
+       "city": "San Péter",
+         "country": "México",
+         "firstName": "Valentín2.",
+         "lastName": "Dmitrovích2.",
+         "phoneNumber": "811-965-4348-",
+         "email":"review@netpay.com.mx",
+         "postalCode": "66240",
+         "state": "NUEVO LÉON",
+         "street1": "Hidalgo 627._éÉ",
+         "street2": "Hidalgo 627._éÉ",
+         "ipAddress": "127.0.0.1"
+        },
+        "ship":{  
+           "city":"city ship",
+           "country":"MX",
+           "firstName":"mailto",
+       "lastName":"mailto",
+           "phoneNumber":"8110111111",
+           "postalCode":"12345",
+           "state":"state",
+           "street1":"street 1",
+           "street2":"street 2",
+           "shippingMethod":"flatrate_flatrate"
+        },
+        "itemList":[{  
+              "id":"421",
+              "productSKU":"wbk012c-Royal Blue-S",
+              "unitPrice":"1.0000",
+              "productName":"Elizabeth Knit Top",
+              "quantity":1,
+              "productCode":"Tops & Blouses"
+           }
+        ],
+        "card":{  
+           "cardToken":"VSRr14Fe5CThE50FhUNT6vNAM8gzAuZNXn5vSTPwRzMH+yRdDt3v5a9ct7nuBzAvrftOEvBrl71vJbx67vWjiudz4oenzirIQyClN8+hTRM="
+        },
+    "purchaseTotals":{  
+           "grandTotalAmount":"6",
+           "currency":"MXN"
+        },
+        "merchanDefinedDataList":[
+    {
+      "id": "2",
+      "value": "Web"
+    },
+    {
+      "id": "4",
+      "value": "515"
+    },
+    {
+      "id": "5",
+      "value": "0"
+    },
+    {
+      "id": "6",
+      "value": "0"
+    },
+    {
+      "id": "7",
+      "value": "0"
+    },
+    {
+      "id": "9",
+      "value": "Retail"
+    },
+    {
+      "id": "10",
+      "value": "3D"
+    },
+    {
+      "id": "11",
+      "value": "flatrate_flatrate"
+    },
+    {
+      "id": "13",
+      "value": "N"
+    },
+    {
+      "id": "14",
+      "value": "Domicilio"
+    },
+    {
+      "id": "16",
+      "value": "50000"
+    },
+    {
+      "id": "8",
+      "value": "7369687"
+    },
+    {
+      "id": "17",
+      "value": "7369687"
+    },
+    {
+      "id": "18",
+      "value": "1"
+    }]
+     }
+  }
+}
 
     def get_jwt_token(self):
         response = netpay_ecommerce.AuthJwt.create(self.login_object)
